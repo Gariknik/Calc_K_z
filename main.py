c776dc17e59e8a04bb382e594c3188f111a697e9
Gariknik <Nikitenkivi@gmail.com>
2025-02-08 14:02:08 +0200

    My first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from openpyxl import Workbook
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=200)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
